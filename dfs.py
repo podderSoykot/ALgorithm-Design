2922bc036216b360863319b6f681118c5378aa29
podderSoykot <diptopodder95@gmail.com>
2024-06-07 19:06:32 +0600

    DFS Created

---
+++ dfs.py
@@ -0,0 +1,5 @@
+"""
+Depth first search(DFS): Is an fundamental graph traversal algortithm, 
+commonly used to explore nodes and edges in a graph. Unlike BFS, which 
+explores nodes layer by layer, DFS drive as deep as
+"""
